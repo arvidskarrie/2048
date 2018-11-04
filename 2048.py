ad21fced3fafc9810ecb72ce18d3e91f5df84621
arvidskarrie <44729270+arvidskarrie@users.noreply.github.com>
2018-11-04 22:50:47 +0100

    Implement moveDown

--- 2048.py
+++ 2048.py
@@ -14,53 +14,50 @@ from builtins import print
 
 
 class game_of_2048:
-    
     def __init__(self):
         self.initiate_board()
-        #self.print_board()
     
     def initiate_board(self):
         random.seed()
         
         self.board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.insert_brick()
-        
-       
     
     def insert_brick(self):
-        
         if 0 in self.board:
-            print('0 is in list')
             random_brick = random.randint(0,15)
             
             while (self.board[random_brick] != 0):
                 random_brick = random.randint(0,15)
                 
             insert_value = 2 if (random.random() > 0.1) else 4
-            
             self.board[random_brick] = insert_value
-            
             self.print_board()
         else:
             self.print_board()
             print('game lost')
             quit(0)
-                
     
     def print_board(self):
         for i in range(4):
             print(self.board[4 * i:4 * (i + 1)])
         print('---------')
+            
+    def moveUp(self):
+        self.pack_n_merge()
+        self.insert_brick()
         
+    def moveDown(self): 
+        self.rotate(2)
+        self.pack_n_merge()
+        self.rotate(2)
+        self.insert_brick()
+        
+    def pack_n_merge(self):
+        self.pack()
+        self.merge() 
+        self.pack()
     
-#     def pack_one_row(self, row):
-#         for column in range(4):
-#             repetitions = 0
-#             while (repetitions < 4):
-#                 if (self.board[4 * row + columns] == 0):
-#                     for row_below_original row
-#                     self.board[4 * row + columns] =
-        
     def pack(self):
         for column in range(4):
             for repetitions in range(3):
@@ -75,25 +72,26 @@ class game_of_2048:
                 if self.board[4 * row + column] == self.board[4 * (row + 1) + column]:
                     self.board[4 * row + column] *= 2
                     self.board[4 * (row + 1) + column] = 0
+                        
+    def rotate(self, direction):
+        #if direction == 1: #90 degrees clockwise
             
+        if direction == 2: # Down
+            newBoard = []
+            for i in range(16):
+                newBoard.append(self.board[15-i])
         
-            
-    def moveUp(self): 
-        self.pack()
-        self.merge() 
-        self.pack()
-        print('insert  brick:')
-        self.insert_brick()
         
+        self.board = newBoard 
     
-        
     
 def main():
     board = game_of_2048()
     for move in range(100):
         print('Move', move)
         board.moveUp()
-    #board.moveLeft
+        board.moveDown()
+    
     print('quit')
 
 main()
